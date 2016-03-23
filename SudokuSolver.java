ddb305747af030ca87d3522e74761d9a09404e9d
Bkurq <bert2030@gmail.com>
2016-03-23 15:51:37 +0100

    Decided on architecture

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -3,6 +3,8 @@ public class SudokuSolver {
     private int[][] originalTable;
 
     public SudokuSolver(int[][] table) {
+        if(!(table.length == 9 && table[0].length == 9))
+            throw new IllegalArgumentException("The sudoku table must 9 x 9");
         originalTable = (int[][]) table.clone();
         workingTable = (int[][]) table.clone();
     }
@@ -11,8 +13,29 @@ public class SudokuSolver {
         return solve(0, 0);
     }
 
-    private boolean solve(int x, int y) {
+    /**
+     * Recursive method that solves sudoku with backtracking
+     * @param row
+     * @param col
+     * @return
+     */
+    private boolean solve(int row, int col) {
+        if(originalTable[row][col] != 0)
+            if(checkRules(row, col)) {
+                solve(row, col + 1);
+            } else {
+                return false;
+            }
         return false;
     }
 
+    /**
+     * Checks if the number in workingTable at position specified by row and col fulfills rules of sudoku
+     * @param row
+     * @param col
+     * @return
+     */
+    private boolean checkRules(int row, int col) {
+        return false;
+    }
 }
